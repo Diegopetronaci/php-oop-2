b118efd35bf00ec3129ad888535724746d53d166
Diego <Petronacidiego@gmail.com>
2021-02-17 15:25:20 +0100

    esercizio sistemato

--- classes/UserProfile.php
+++ classes/UserProfile.php
@@ -1,22 +1,24 @@
 <?php
 
-class UserProfile extends User {
-    public $nome;
-    public $cognome;
-    public $data_di_nascita;
-    public $cell;
-    public $biografia;
+    include_once __DIR__ . '/User.php';
+    
+    class UserProfile extends User {
+        public $nome;
+        public $cognome;
+        public $data_di_nascita;
+        public $cell;
+        public $biografia;
 
-    public function __construct(string $nome, string $cognome, string $data_di_nascita, int $cell, string $biografia, string $username, string $email, string $password) {
-        
-        parent::__construct($username, $email, $password);
-        
-        $this->nome = $nome;
-        $this->cognome = $cognome;
-        $this->data_di_nascita = $data_di_nascita;
-        $this->cell = $cell;
-        $this->biografia = $biografia;
-    }
+        public function __construct(string $username, string $email, string $password, string $nome, string $cognome, string $data_di_nascita, string $cell, string $biografia) {
+            
+            parent::__construct($username, $email, $password);
+            
+            $this->nome = $nome;
+            $this->cognome = $cognome;
+            $this->data_di_nascita = $data_di_nascita;
+            $this->cell = $cell;
+            $this->biografia = $biografia;
+        }
 }
 
 
--- index.php
+++ index.php
@@ -3,11 +3,17 @@
     include __DIR__ . '/classes/User.php';
     include __DIR__ . '/classes/UserProfile.php';
 
-    $utenti = [
-    //new User('Diego', 'Petronaci', "09/01/1999", 3314215613, 'Biografia', 'xDIEGOitax', 'petronacidiego@gmail.com', 'password'),
+    /* $utenti = [
+        //new UserProfile('Diego', 'Petronaci', "09/01/1999", 3314215613, 'Biografia', 'xDIEGOitax', 'petronacidiego@gmail.com', 'password'),
         new User('xDIEGOitax', 'diego@gmail.com', 'password'),
         new User('Biasox', 'tommaso@gmail.com', 'password'),
         new User('Faber', 'fabrizio@gmail.com', 'password')
+    ]; */
+
+    $utenti = [
+        new UserProfile('xDIEGOitax', 'Diego@gmail.com', 'Password', 'Diego', 'Petronaci', '09/01/1999', 3314215613, 'bio'),
+        new UserProfile('Biasox', 'tommaso@gmail.com', 'password', 'Tommaso', 'Biasini', '09/01/1999', 3314215613, 'bio'),
+        new UserProfile('Faber', 'fabrizio@gmail.com', 'password', 'Fabrizio', 'De Camillis', '09/01/1999', 3314215613, 'bio'),
     ];
 
     /* foreach ($utenti as $utente) {
@@ -31,13 +37,14 @@
     <?php foreach ($utenti as $utente) { ?>
 
         <h2> Utente ID: <?php echo $utente->username; ?> </h2>
-        <!-- <p> Nome: <?php echo $utente->nome; ?> </p>
+        <p> Nome: <?php echo $utente->nome; ?> </p>
         <p> Cognome: <?php echo $utente->cognome; ?> </p>
-        <p> Data di nascita: <?php echo $utente->data_di_nascita; ?> </p> -->
+        <p> Data di nascita: <?php echo $utente->data_di_nascita; ?> </p>
         <p> Email: <?php echo $utente->email; ?> </p>
         <p> Password: <?php echo $utente->password; ?> </p>
 
     <?php } ?>
+
 </body>
 
 </html>
